5c545bc0464497ef88a3e1e46f6b8299c393e14a
Luca Pireddu <pireddu@crs4.it>
2020-05-20 18:52:51 +0200

    Completed CRUD API example

--- lifemonitor/api.py
+++ lifemonitor/api.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
 
-import logging
+import os
 import uuid
 
+import connexion
+import sqlalchemy
+
 from lifemonitor import config, models
-from lifemonitor.config import logger, db
+from lifemonitor.config import logger
+from lifemonitor.models import db
 
 WORK = {}
+
+connex_app = None
+
+def _row_to_dict(row):
+    d = {}
+    for column in row.__table__.columns:
+        d[column.name] = getattr(row, column.name)
+    return d
+
 
 def workflows_get():
     results = models.Workflow.query.all()
     logger.debug("workflows_get. Got %s workflows", len(results))
-    return results
+    return [ _row_to_dict(row) for row in results ]
 
 
 def workflows_post(body):
@@ -26,28 +39,61 @@ def workflows_post(body):
     return str(w.workflow_id), 201
 
 
+def workflows_put(wf_id, body):
+    logger.debug("PUT called for wf_id %s", wf_id)
+    try:
+        wf = models.Workflow.query.get(wf_id)
+    except sqlalchemy.exc.DataError:
+        return "Invalid ID", 400
+    wf.name = body['name']
+    db.session.commit()
+    return connexion.NoContent, 200
+
+
 def workflows_get_by_id(wf_id):
-    if wf_id in WORK:
-        return WORK[wf_id]
-    return 404
+    try:
+        wf = models.Workflow.query.get(wf_id)
+    except sqlalchemy.exc.DataError:
+        return "Invalid ID", 400
+
+    if wf is not None:
+        # Once we customize the JSON encoder or implement a smarter serialization
+        # with Marshmellow we could simply return the value
+        return _row_to_dict(wf)
+
+    return connexion.NoContent, 404
 
 
 def workflows_delete(wf_id):
-    if wf_id in WORK:
-        del WORK[wf_id]
-    return 204
+    try:
+        wf = models.Workflow.query.get(wf_id)
+    except sqlalchemy.exc.DataError:
+        return "Invalid ID", 400
+
+    if wf is None:
+        return connexion.NoContent, 200
+
+    db.session.delete(wf)
+    db.session.commit()
+    logger.debug("Committed")
+    return connexion.NoContent, 204
 
 
-def main():
-    logger.info("Starting application")
-    logger.debug(
-        "REMOVE THIS LOG CALL - it includes the password! DB URI: %s",
-        config.flask_app.config['SQLALCHEMY_DATABASE_URI'])
-    db.create_all()
-    config.connex_app.add_api('api.yaml', validate_responses=True)
-    config.connex_app.run(port=8080)
+def create_app():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    global connex_app
+    connex_app = connexion.App('LM', specification_dir=base_dir)
+    flask_app = connex_app.app
+
+    with flask_app.app_context():
+        config.configure_logging(flask_app)
+        logger.info("Starting application")
+
+        models.config_db_access(flask_app)
+
+    connex_app.add_api('api.yaml', validate_responses=True)
+    connex_app.run(port=8080)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    main()
+    create_app()
--- lifemonitor/models.py
+++ lifemonitor/models.py
@@ -1,12 +1,39 @@
 
+import os
 import uuid
+
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID
 
-from .config import db
+db = SQLAlchemy()
+
+
+def db_uri():
+    if os.getenv('DATABASE_URI'):
+        uri = os.getenv('DATABASE_URI')
+    else:
+        uri = "postgresql://{user}:{passwd}@{host}/{dbname}".format(
+            user=os.getenv('POSTGRESQL_USERNAME', 'lm'),
+            passwd=os.getenv('POSTGRESQL_PASSWORD', ''),
+            host=os.getenv('POSTGRESQL_HOST', 'db'),
+            dbname=os.getenv('POSTGRESQL_DATABASE'))
+    return uri
+
+
+def config_db_access(flask_app):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri()
+    # FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds significant
+    # overhead and will be disabled by default in the future.  Set it to True
+    # or False to suppress this warning.
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(flask_app)
+    db.create_all()
+
 
 class Workflow(db.Model):
     workflow_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
-        return '<Workflow %r; name: %r>'.format(self.workflow_id, self.name)
+        return '<Workflow {:r}; name: {:r}>'.format(self.workflow_id, self.name)
