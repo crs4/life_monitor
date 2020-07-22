79b815447c80a3bbdc8f4ae897760f90ab5013a7
Marco Enrico Piras <kikkomep@crs4.it>
2020-07-22 16:40:15 +0200

    Use Gunicorn in production

--- docker/lm_entrypoint.sh
+++ docker/lm_entrypoint.sh
@@ -9,4 +9,12 @@ export POSTGRESQL_DATABASE="${POSTGRESQL_DATABASE:-lm}"
 printf "Waiting for postgresql...\n" >&2
 wait-for-postgres.sh
 printf "DB is ready.  Starting application\n" >&2
-python3 "${HOME}/lifemonitor/api.py"
+if [[ "${DEV}" == "true" ]]; then
+  printf "Staring app in DEV mode (Flask built-in web server with auto reloading)"
+  python "${HOME}/lifemonitor/api.py"
+else
+  gunicorn --workers "${GUNICORN_WORKERS}"  \
+           --threads "${GUNICORN_THREADS}" \
+           -b "0.0.0.0:8000" \
+           "lifemonitor.api:create_app()"
+fi
