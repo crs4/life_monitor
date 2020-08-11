600f39f37375fef498dd92ad0460df6f8c103599
Marco Enrico Piras <kikkomep@crs4.it>
2020-08-12 00:42:35 +0200

    Add 'db init' command

---
+++ lifemonitor/commands/db.py
@@ -0,0 +1,21 @@
+import logging
+from flask import Blueprint, current_app, g
+from flask.cli import with_appcontext
+
+# set module level logger
+logger = logging.getLogger(__name__)
+
+# define the blueprint for DB commands
+blueprint = Blueprint('db', __name__)
+
+
+@blueprint.cli.command('init')
+@with_appcontext
+def db_init():
+    """
+    Initialize the DB
+    :return:
+    """
+    from lifemonitor.app import db
+    logger.debug("Initializing DB...")
+    db.create_all()
