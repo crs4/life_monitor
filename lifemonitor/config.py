16b75325bed286216db3ff9b5df622668da354b5
Marco Enrico Piras <kikkomep@crs4.it>
2020-08-11 14:22:31 +0200

    Define configuration classes

--- lifemonitor/config.py
+++ lifemonitor/config.py
@@ -1,29 +1,8 @@
 import os
 import logging
-
-logger = logging.getLogger('lm')
+from typing import List, Type
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-def is_debug_mode_enabled():
-    return os.environ.get("DEBUG", False)
-
-
-def configure_logging(app):
-    level_str = app.config.get('LOG_LEVEL', 'INFO')
-    error = False
-    try:
-        level_value = getattr(logging, level_str)
-    except AttributeError:
-        level_value = logging.INFO
-        error = True
-
-    logging.basicConfig(level=level_value)
-    if error:
-        app.logger.error("LOG_LEVEL value %s is invalid. Defaulting to INFO", level_str)
-
-    app.logger.info('Logging is active. Log level: %s', logging.getLevelName(app.logger.getEffectiveLevel()))
 
 
 def db_uri():
@@ -31,6 +10,7 @@ def db_uri():
     Build URI to connect to the DataBase
     :return:
     """
+    # "sqlite:///{0}/app-dev.db".format(basedir)
     if os.getenv('DATABASE_URI'):
         uri = os.getenv('DATABASE_URI')
     else:
@@ -51,10 +31,78 @@ def config_db_access(flask_app, db):
     :return:
     """
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri()
-    # FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds significant
-    # overhead and will be disabled by default in the future.  Set it to True
-    # or False to suppress this warning.
-    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(flask_app)
     db.create_all()
+
+
+class BaseConfig:
+    CONFIG_NAME = "base"
+    USE_MOCK_EQUIVALENCY = False
+    DEBUG = False
+    LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
+    # Add a random secret (required to enable HTTP sessions)
+    SECRET_KEY = os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = db_uri()
+    # FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds significant
+    # overhead and will be disabled by default in the future.  Set it to True
+    # or False to suppress this warning.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    CONFIG_NAME = "development"
+    # Add a random secret (required to enable HTTP sessions)
+    SECRET_KEY = os.getenv(
+        "DEV_SECRET_KEY", "You can't see California without Marlon Widgeto's eyes"
+    )
+    DEBUG = True
+    LOG_LEVEL = "DEBUG"
+    TESTING = False
+
+
+class ProductionConfig(BaseConfig):
+    CONFIG_NAME = "production"
+    SECRET_KEY = os.getenv("PROD_SECRET_KEY", "I'm Ron Burgundy?")
+    DEBUG = False
+    TESTING = False
+
+
+class TestingConfig(BaseConfig):
+    CONFIG_NAME = "testing"
+    SECRET_KEY = os.getenv("TEST_SECRET_KEY", "Thanos did nothing wrong")
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///{0}/app-test.db".format(basedir)
+
+
+_EXPORT_CONFIGS: List[Type[BaseConfig]] = [
+    DevelopmentConfig,
+    TestingConfig,
+    ProductionConfig,
+]
+_config_by_name = {cfg.CONFIG_NAME: cfg for cfg in _EXPORT_CONFIGS}
+
+
+def get_config_by_name(name):
+    try:
+        return _config_by_name[name]
+    except KeyError:
+        return ProductionConfig
+
+
+def configure_logging(app):
+    level_str = app.config.get('LOG_LEVEL', 'INFO')
+    error = False
+    try:
+        level_value = getattr(logging, level_str)
+    except AttributeError:
+        level_value = logging.INFO
+        error = True
+
+    logging.basicConfig(level=level_value)
+    if error:
+        app.logger.error("LOG_LEVEL value %s is invalid. Defaulting to INFO", level_str)
+
+    app.logger.info('Logging is active. Log level: %s', logging.getLevelName(app.logger.getEffectiveLevel()))
