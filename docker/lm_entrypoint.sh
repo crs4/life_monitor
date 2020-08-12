0f7e9ac975ea2c677b45f702ec070b42d08e37f0
Marco Enrico Piras <kikkomep@crs4.it>
2020-08-12 09:57:56 +0200

    Fix Docker image entrypoint

--- docker/lm_entrypoint.sh
+++ docker/lm_entrypoint.sh
@@ -11,10 +11,10 @@ wait-for-postgres.sh
 printf "DB is ready.  Starting application\n" >&2
 if [[ "${FLASK_ENV}" == "development" ]]; then
   printf "Staring app in DEV mode (Flask built-in web server with auto reloading)"
-  python "${HOME}/run.py"
+  python "${HOME}/app.py"
 else
   gunicorn --workers "${GUNICORN_WORKERS}"  \
            --threads "${GUNICORN_THREADS}" \
            -b "0.0.0.0:8000" \
-           "lifemonitor.app:create_app()"
+           "app"
 fi
