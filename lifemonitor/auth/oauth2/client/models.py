977a8602c1addb6df9d8c6c6337a25155c71b759
Marco Enrico Piras <kikkomep@crs4.it>
2020-08-21 12:48:39 +0200

    Register routes to handle oauth2 authentication

--- lifemonitor/auth/oauth2/client/models.py
+++ lifemonitor/auth/oauth2/client/models.py
@@ -1,3 +1,27 @@
+from __future__ import annotations
 
 
+class OAuthUserProfile(object):
 
+    def __init__(self, sub=None, name=None, email=None, preferred_username=None,
+                 profile=None, picture=None, website=None) -> None:
+        self.sub = sub
+        self.name = name
+        self.email = email
+        self.preferred_username = preferred_username
+        self.profile = profile
+        self.picture = picture
+        self.website = website
+
+    def to_dict(self):
+        res = {}
+        for k in ['sub', 'name', 'email', 'preferred_username', 'profile', 'picture', 'website']:
+            res[k] = getattr(self, k)
+        return res
+
+    @staticmethod
+    def from_dict(data: dict):
+        profile = OAuthUserProfile()
+        for k, v, in data.items():
+            setattr(profile, k, v)
+        return profile
