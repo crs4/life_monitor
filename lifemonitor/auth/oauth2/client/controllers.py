8ff6115ff9abb542c5478341cc769a31db4ab4cd
Marco Enrico Piras <kikkomep@crs4.it>
2020-08-22 11:30:15 +0200

    Fix registration of OAuth2 providers

--- lifemonitor/auth/oauth2/client/controllers.py
+++ lifemonitor/auth/oauth2/client/controllers.py
@@ -6,7 +6,7 @@ from authlib.integrations.base_client import RemoteApp
 from loginpass import create_flask_blueprint
 
 from .models import OAuthUserProfile
-from .services import oauth2_registry, oauth2_backends
+from .services import oauth2_registry
 
 # Config a module level logger
 logger = logging.getLogger(__name__)
@@ -16,4 +16,4 @@ def create_blueprint(handle_authorize):
     def _handle_authorize(provider: RemoteApp, token, user_info):
         return handle_authorize(provider, token, OAuthUserProfile.from_dict(user_info))
 
-    return create_flask_blueprint(oauth2_backends, oauth2_registry, _handle_authorize)
+    return create_flask_blueprint([], oauth2_registry, _handle_authorize)
