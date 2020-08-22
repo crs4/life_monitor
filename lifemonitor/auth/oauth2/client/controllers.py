e7d17a14e70945b112988f2d7d57ad66482aea6e
Marco Enrico Piras <kikkomep@crs4.it>
2020-08-22 02:01:18 +0200

    Fix OAuth2 registry initialization

--- lifemonitor/auth/oauth2/client/controllers.py
+++ lifemonitor/auth/oauth2/client/controllers.py
@@ -5,20 +5,15 @@ import logging
 from authlib.integrations.base_client import RemoteApp
 from loginpass import create_flask_blueprint
 
-from lifemonitor.auth.oauth2.client.providers.github import GitHub
-from lifemonitor.auth.oauth2.client.providers.seek import Seek
 from .models import OAuthUserProfile
-from .services import oauth2_registry
+from .services import oauth2_registry, oauth2_backends
 
 # Config a module level logger
 logger = logging.getLogger(__name__)
-
-#: you can customize this part
-backends = [GitHub, Seek]
 
 
 def create_blueprint(handle_authorize):
     def _handle_authorize(provider: RemoteApp, token, user_info):
         return handle_authorize(provider, token, OAuthUserProfile.from_dict(user_info))
 
-    return create_flask_blueprint(backends, oauth2_registry, _handle_authorize)
+    return create_flask_blueprint(oauth2_backends, oauth2_registry, _handle_authorize)
--- lifemonitor/auth/oauth2/client/services.py
+++ lifemonitor/auth/oauth2/client/services.py
@@ -1,9 +1,56 @@
+from __future__ import annotations
 import logging
 
 from authlib.integrations.flask_client import OAuth
+from authlib.integrations.flask_client import FlaskRemoteApp
+from authlib.oauth2.rfc6749 import OAuth2Token
+from flask_login import current_user
+from flask import current_app
+
+from .providers.seek import Seek
+from .providers.github import GitHub
 
 # Config a module level logger
+from ...models import OAuthIdentity
+
 logger = logging.getLogger(__name__)
 
+
+def fetch_token(name):
+    logger.debug("NAME: %s", name)
+    logger.debug("CURRENT APP: %r", current_app.config)
+    api_key = current_app.config.get("{}_API_KEY".format(name.upper()), None)
+    if api_key:
+        logger.debug("FOUND an API KEY for the OAuth Service '%s': %s", name, api_key)
+        return {"access_token": api_key}
+    identity = OAuthIdentity.find_by_provider(current_user.id, name)
+    logger.debug("The token: %r", identity.token)
+    return OAuth2Token(identity.token)
+
+
+def update_token(name, token, refresh_token=None, access_token=None):
+    if access_token or refresh_token:
+        identity = OAuthIdentity.find_by_provider(current_user.id, name)
+    else:
+        return
+
+    # update old token
+    identity.token = token
+    identity.save()
+
+
 # Create an instance of OAuth registry for oauth clients.
-oauth2_registry = OAuth()
+oauth2_registry = OAuth(fetch_token=fetch_token, update_token=update_token)
+
+# Register backend services
+oauth2_backends = [GitHub, Seek]
+for backend in oauth2_backends:
+    class RemoteApp(backend, FlaskRemoteApp):
+        OAUTH_APP_CONFIG = backend.OAUTH_CONFIG
+
+
+    oauth2_registry.register(RemoteApp.NAME, overwrite=True, client_cls=RemoteApp)
+
+
+class OAuth2Registry(OAuth):
+    pass
