c28a852274e1ca2edf5bee2bb6df076be05fdd7c
Marco Enrico Piras <kikkomep@crs4.it>
2020-08-22 15:10:42 +0200

    Add OAuth2Registry to the app context

--- lifemonitor/auth/oauth2/client/__init__.py
+++ lifemonitor/auth/oauth2/client/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
+from flask import g
 from .controllers import create_blueprint
 from .services import oauth2_registry
 
@@ -9,4 +10,8 @@ logger = logging.getLogger(__name__)
 
 def register_api(app, specs_dir, handle_authorize):
     oauth2_registry.init_app(app)
+    # Register the OAuth2Registry into the current context
+    with app.app_context():
+        g.oauth2_registry = oauth2_registry
+    # Register the '/oauth2' endpoint
     app.register_blueprint(create_blueprint(handle_authorize), url_prefix="/oauth2")
