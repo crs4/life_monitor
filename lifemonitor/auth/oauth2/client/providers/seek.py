ef5c64fc211fbf9fdc0503a0355446257984fe24
Marco Enrico Piras <kikkomep@crs4.it>
2020-08-25 17:13:56 +0000

    Fix Seek provider settings

--- lifemonitor/auth/oauth2/client/providers/seek.py
+++ lifemonitor/auth/oauth2/client/providers/seek.py
@@ -1,4 +1,6 @@
+import os
 import logging
+from flask import current_app
 
 # Config a module level logger
 logger = logging.getLogger(__name__)
@@ -21,14 +23,24 @@ def normalize_userinfo(client, data):
 
 class Seek(object):
     NAME = 'seek'
+    # define API URLs
+    _api_base_url = current_app.config.get(
+        "{}_API_BASE_URL".format(NAME.upper()), 'https://workflowhub.eu')
+    _api_urls = (
+        ('API_BASE_URL', _api_base_url),
+        ('ACCESS_TOKEN_URL', os.path.join(_api_base_url, '/oauth/token')),
+        ('AUTHORIZE_URL', os.path.join(_api_base_url, '/oauth/authorize')),
+        ('USERINFO_ENDPOINT', os.path.join(_api_base_url, '/people/current?format=json'))
+    )
+    # init the OAuth configuration with static settings
     OAUTH_CONFIG = {
-        'api_base_url': 'https://rachk8s.me:3000',
-        'access_token_url': 'oauth/token',
-        'authorize_url': 'oauth/authorize',
         'client_kwargs': {'scope': 'read'},
-        'userinfo_endpoint': 'https://rachk8s.me:3000/people/current?format=json',
         'userinfo_compliance_fix': normalize_userinfo,
     }
+	# append the API urls to the configuration
+    for url in _api_urls:
+        OAUTH_CONFIG[url[0].lower()] = current_app.config.get(
+            "{}_{}".format(NAME.upper(), url[0]), url[1])
 
 
 def refresh_oauth2_token(func):
