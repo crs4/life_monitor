347943b657b52aca0a26d16dc8469927d3deb938
Marco Enrico Piras <kikkomep@crs4.it>
2020-09-04 08:46:55 +0200

    Add initial utility function to search for the test_definition_file inside a RO-crate

--- lifemonitor/utils.py
+++ lifemonitor/utils.py
@@ -6,8 +6,10 @@ import zipfile
 
 import flask
 import requests
+from flask import url_for
 
 RO_CRATE_METADATA_FILENAME = "ro-crate-metadata.jsonld"
+RO_CRATE_TEST_DEFINITION_FILENAME = "test-suite-definition.json"
 
 logger = logging.getLogger()
 
@@ -58,6 +60,44 @@ def load_ro_crate_metadata(roc_path):
     with open(file_path) as data_file:
         logger.info("Loading RO Crate Metadata @ %s", file_path)
         return json.load(data_file)
+
+
+def load_test_definition_filename(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+
+def search_for_test_definition(roc_path, ro_crate_metadata: dict):
+    # first search on the root roc_path for a test_definition file
+    filename = os.path.join(roc_path, RO_CRATE_TEST_DEFINITION_FILENAME)
+    if os.path.exists(filename):
+        return load_test_definition_filename(filename)
+    # check for if there exists a ZIP archive as root Dataset containing the actual RO crate
+    graph = ro_crate_metadata.get("@graph", None)
+    if not graph:
+        return None
+
+    mid = main_entity = None
+    for node in graph:
+        nid = node.get("@id", None)
+        ntype = node.get("@type", None)
+        if nid and nid == "./" and ntype and ntype == "Dataset":
+            main_entity = node.get("mainEntity", None)
+            break
+
+    if main_entity:
+        mid = main_entity.get("@id", None)
+        if not mid:
+            return None
+
+    # check if the main_entity if a ZIP file
+    if not mid.endswith(".zip"):
+        return None
+    dataset_path = extract_zip(os.path.join(roc_path, mid))
+    print("Dataset path: %s", dataset_path)
+    dataset_metadata = load_ro_crate_metadata(dataset_path)
+    print("Dataset meatadata: %r", dataset_metadata)
+    return search_for_test_definition(dataset_path, dataset_metadata)
 
 
 def push_request_to_session(name):
