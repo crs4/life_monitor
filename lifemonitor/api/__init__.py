bda049de5bd5ed757b88f7112648129c18b4877a
Marco Enrico Piras <kikkomep@crs4.it>
2020-09-24 14:59:05 +0000

    Enable response validation

--- lifemonitor/api/__init__.py
+++ lifemonitor/api/__init__.py
@@ -5,6 +5,7 @@ from .serializers import ma
 
 def register_api(app, specs_dir):
     api = connexion.Api(pathlib.Path(specs_dir, 'api.yaml'),
+                        validate_responses=True,
                         arguments={'global': 'global_value'})
     app.register_blueprint(api.blueprint)
     ma.init_app(app)
