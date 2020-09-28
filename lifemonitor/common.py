9f0fb2beccf9acbad4f97f47aadff1e7608069f0
Marco Enrico Piras <kikkomep@crs4.it>
2020-09-28 21:23:07 +0000

    Serialize LifeMonitor exceptions/errors as ProblemDetails

--- lifemonitor/common.py
+++ lifemonitor/common.py
@@ -1,47 +1,149 @@
+from flask import Response, request
+from lifemonitor import serializers
+
+
 class LifeMonitorException(Exception):
+
+    def __init__(self, title=None, detail="",
+                 type="about:blank", status=500, instance=None, **kwargs):
+        self.title = title
+        self.detail = detail
+        self.type = type
+        self.status = status
+        self.instance = instance
+        self.extra_info = kwargs
+
+        if instance is None:
+            try:
+                self.instance = request.url
+            except Exception:
     pass
+
+    def to_json(self):
+        return serializers.ProblemDetailsSchema().dumps(self)
 
 
 class NotImplementedException(LifeMonitorException):
-    pass
+
+    def __init__(self, title="Not Implemented", detail="",
+                 type="about:blank", status=501, instance=None, **kwargs):
+        super().__init__(title, detail, type, status, instance, **kwargs)
 
 
 class UnsupportedOperationException(LifeMonitorException):
-    pass
+
+    def __init__(self, detail="The operation has not been implemented yet",
+                 type="about:blank", status=501, instance=None, **kwargs):
+        super().__init__(title="Not Implemented",
+                         detail=detail, status=status, **kwargs)
 
 
 class NotAuthorizedException(LifeMonitorException):
-    pass
+    def __init__(self, detail="",
+                 type="about:blank", status=401, instance=None, **kwargs):
+        super().__init__(title="Unauthorized",
+                         detail=detail, status=status, **kwargs)
+
+
+class Forbidden(LifeMonitorException):
+    def __init__(self, detail="",
+                 type="about:blank", status=403, instance=None, **kwargs):
+        super().__init__(title="Forbidden",
+                         detail=detail, status=status, **kwargs)
 
 
 class SpecificationNotDefinedException(LifeMonitorException):
-    pass
+
+    def __init__(self, detail="Specification not defined",
+                 type="about:blank", status=400, instance=None, **kwargs):
+        super().__init__(title="Bad request",
+                         detail=detail, status=status, **kwargs)
 
 
 class SpecificationNotValidException(LifeMonitorException):
-    pass
+
+    def __init__(self, detail="Invalid specification",
+                 type="about:blank", status=400, instance=None, **kwargs):
+        super().__init__(title="Bad request",
+                         detail=detail, status=status, **kwargs)
 
 
 class EntityNotFoundException(LifeMonitorException):
 
-    def __init__(self, entity_class, entity_id=None) -> None:
-        super().__init__()
+    def __init__(self, entity_class, entity_id=None, **kwargs) -> None:
+        detail = f"{entity_class.__name__} with id {entity_id} not found" \
+            if entity_id else f"{entity_class.__name__} not found"
+        kwargs["resource_type"] = entity_class.__name__
+        if entity_id:
+            kwargs["resource_identifier"] = entity_id
+        super().__init__(
+            title="Resource not found",
+            detail=detail, status=404, **kwargs)
         self.entity_class = entity_class
         self.entity_id = entity_id
 
     def __str__(self):
-        if self.entity_id:
-            return "{} with id {} not found".format(self.entity_class.__name__, self.entity_id)
-        return "{} not found".format(self.entity_class.__name__)
+        return self.detail
+
+
+class NotValidROCrateException(LifeMonitorException):
+
+    def __init__(self, detail="Not valid RO Crate",
+                 type="about:blank", status=501, instance=None, **kwargs):
+        super().__init__(title="Bad request",
+                         detail=detail, status=status, **kwargs)
 
 
 class TestingServiceNotSupportedException(LifeMonitorException):
+
+    def __init__(self, detail="Testing service not supported",
+                 type="about:blank", status=400, instance=None, **kwargs):
+        super().__init__(title="Bad request",
+                         detail=detail, status=status, **kwargs)
+
+
+class TestingServiceException(LifeMonitorException):
+
+    def __init__(self, detail="Testing service not supported",
+                 type="about:blank", status=500, instance=None, **kwargs):
+        super().__init__(title="Internal Server Error",
+                         detail=detail, status=status, **kwargs)
+
+
+def handle_exception(e: Exception):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    if isinstance(e, LifeMonitorException):
+        return Response(response=e.to_json(),
+                        status=e.status,
+                        mimetype="application/problem+json")
+    else:
+        return report_problem(status=500,
+                              title="Internal Server Error",
+                              detail=getattr(e, "description", None))
+
+
+def report_problem(status, title, detail, type=None, instance=None, extra_info=None):
+    """
+    Returns a `Problem Details <https://tools.ietf.org/html/draft-ietf-appsawg-http-problem-00>`_ error response.
+    """
+    if not type:
+        type = 'about:blank'
+
+    problem_response = {'type': type, 'title': title, 'detail': detail, 'status': status}
+    if instance:
+        problem_response['instance'] = instance
+    else:
+        try:
+            problem_response['instance'] = request.url
+        except Exception:
     pass
+    if extra_info:
+        problem_response['extra_info'] = extra_info
+
+    return Response(response=serializers.ProblemDetailsSchema().dumps(problem_response),
+                    status=status, mimetype="application/problem+json")
 
 
-class NotValidROCrateException(Exception):
-    pass
-
-
-class TestingServiceException(Exception):
-    pass
+def report_problem_from_exception(e: Exception):
+    return handle_exception(e)
