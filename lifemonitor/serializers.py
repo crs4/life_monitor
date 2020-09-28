42397456cead1440d5f72939c3a39aa5eedc42f2
Marco Enrico Piras <kikkomep@crs4.it>
2020-09-28 21:17:20 +0000

    Serialize errors accordingly to the 'ProblemDetails' format (RFC7159)

--- lifemonitor/serializers.py
+++ lifemonitor/serializers.py
@@ -1,6 +1,9 @@
-from marshmallow import pre_load, post_dump, post_load
+from marshmallow import pre_load, post_dump, post_load, fields
 from flask_marshmallow import Marshmallow
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 ma = Marshmallow()
 
@@ -28,3 +31,13 @@ class BaseSchema(ma.SQLAlchemySchema):
     @post_load
     def make_object(self, data, **kwargs):
         return self.__model__(**data) if self.__model__ else data
+
+
+class ProblemDetailsSchema(BaseSchema):
+
+    type = fields.String(default="about:blank")
+    title = fields.String(default="Internal Error")
+    detail = fields.String()
+    status = fields.Integer(default=501)
+    instance = fields.String()
+    extra_info = fields.Dict()
