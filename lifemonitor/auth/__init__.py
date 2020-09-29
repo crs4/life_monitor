d9277d441fc71dba21fdd584a8f6199b09e9bed9
Marco Enrico Piras <kikkomep@crs4.it>
2020-09-29 10:17:14 +0000

    Export auth functions from lifemonitor.auth

--- lifemonitor/auth/__init__.py
+++ lifemonitor/auth/__init__.py
@@ -2,6 +2,12 @@ import logging
 import lifemonitor.auth.oauth2 as oauth2
 from .services import login_manager
 from .controllers import blueprint as auth_blueprint
+from .services import (
+    current_user, current_registry, authorized,
+    login_user, logout_user,
+    login_registry, logout_registry,
+    NotAuthorizedException
+)
 
 # Config a module level logger
 logger = logging.getLogger(__name__)
@@ -13,3 +19,10 @@ def register_api(app, specs_dir):
     oauth2.server.register_api(app, specs_dir)
     app.register_blueprint(auth_blueprint)
     login_manager.init_app(app)
+
+
+__all__ = [
+    register_api, current_user, current_registry, authorized,
+    login_user, logout_user, login_registry, logout_registry,
+    NotAuthorizedException
+]
