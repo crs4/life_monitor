701d527251f27da1c410877e39ad3561cb5a2b3d
simleo <simleo@crs4.it>
2020-10-06 12:58:21 +0200

    update ro-crate-related code and move it to own module

--- interaction_experiments/check_cwl.py
+++ interaction_experiments/check_cwl.py
@@ -23,37 +23,16 @@ Execute a CWL workflow from a crate and check results.
 """
 
 import argparse
-import json
 import os
 import shutil
 import subprocess
 import tempfile
 
 import yaml
+import lifemonitor.ro_crate as roc
 import lifemonitor.test_metadata as tm
 
 YamlDumper = getattr(yaml, "CDumper", getattr(yaml, "Dumper"))
-METADATA_BASENAME = "ro-crate-metadata.jsonld"
-
-
-# https://github.com/workflowhub-eu/about/wiki/Workflow-RO-Crate
-# https://researchobject.github.io/ro-crate/1.0/
-def parse_metadata(crate_dir):
-    fn = os.path.join(crate_dir, METADATA_BASENAME)
-    if not os.path.isfile(fn):
-        raise RuntimeError(f"{METADATA_BASENAME} not found in {crate_dir}")
-    with open(fn, "rt") as f:
-        json_data = json.load(f)
-    entities = {_["@id"]: _ for _ in json_data["@graph"]}
-    md_desc = entities[METADATA_BASENAME]
-    root = entities[md_desc["about"]["@id"]]
-    main = entities[root["mainEntity"]["@id"]]
-    assert main["@type"] == ["File", "SoftwareSourceCode", "Workflow"]
-    # Dataset @id SHOULD end with /
-    return {
-        "main": main["@id"],
-        "test": "test" in [_.rstrip("/") for _ in entities]
-    }
 
 
 # pip install planemo
@@ -82,7 +61,7 @@ def check_workflow(crate_dir, metadata, tests):
 
 
 def main(args):
-    metadata = parse_metadata(args.crate_dir)
+    metadata = roc.parse_metadata(args.crate_dir)
     test_dir = os.path.join(args.crate_dir, "test")
     if not os.path.isdir(test_dir):
         if metadata["test"]:
---
+++ lifemonitor/ro_crate.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2020 CRS4
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""\
+RO-Crate support.
+
+TODO: get rid of this module and depend on ro-crate-py instead once it's stable.
+
+https://www.researchobject.org/ro-crate
+https://github.com/workflowhub-eu/about/wiki/Workflow-RO-Crate
+https://github.com/ResearchObject/ro-crate-py
+"""
+
+import json
+from pathlib import Path
+
+METADATA_BASENAME = "ro-crate-metadata.json"  # since RO-Crate 1.1
+LEGACY_METADATA_BASENAME = "ro-crate-metadata.jsonld"
+
+
+def find_root_entity(entity_map):
+    for entity in entity_map.values():
+        try:
+            conforms_to = entity["conformsTo"]
+        except KeyError:
+            continue
+        if conforms_to.get("@id", "").startswith("https://w3id.org/ro/crate/"):
+            try:
+                root_id = entity["about"]["@id"]
+            except (KeyError, TypeError):
+                continue
+            if root_id in entity_map:
+                return entity_map[root_id]
+    for name in METADATA_BASENAME, LEGACY_METADATA_BASENAME:
+        try:
+            root_id = entity_map[name]["about"]["@id"]
+        except (KeyError, TypeError):
+            continue
+        if root_id in entity_map:
+            return entity_map[root_id]
+    raise ValueError("root data entity not found in crate")
+
+
+def parse_metadata(crate_dir):
+    crate_dir = Path(crate_dir)
+    metadata_path = crate_dir / METADATA_BASENAME
+    if not metadata_path.is_file():
+        metadata_path = crate_dir / LEGACY_METADATA_BASENAME
+        if not metadata_path.is_file():
+            raise RuntimeError(f"{metadata_path} not found")
+    with open(metadata_path, "rt") as f:
+        json_data = json.load(f)
+    entities = {_["@id"]: _ for _ in json_data["@graph"]}
+    root = find_root_entity(entities)
+    print("root:", root)
+    main = entities[root["mainEntity"]["@id"]]
+    assert main["@type"] == ["File", "SoftwareSourceCode", "Workflow"]
+    # Dataset @id SHOULD end with /
+    return {
+        "main": main["@id"],
+        "test": "test" in [_.rstrip("/") for _ in entities]
+    }
