221095602a8ae4337e2a5949aa4a0c1a66168ce0
Marco Enrico Piras <kikkomep@crs4.it>
2020-10-06 10:39:38 +0000

    Fix image build cmd

--- utils/certs/gencerts.sh
+++ utils/certs/gencerts.sh
@@ -4,19 +4,20 @@ CA_NAME="ca"
 LOCALIP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p;' | sed -e ':a;N;$!ba;s/\n/,/g')
 DOMAINS="lm,lm.local,lifemonitor,lifemonitor.local,localhost,seek,nginx,wfhub"
 IPADDRESSES="${LOCALIP}"
+IMAGE_NAME="crs4/minica"
 
 # script path
 current_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # gen cmd
 cmd="minica -ca-cert \"${CA_NAME}.pem\" -ca-key \"${CA_NAME}.key\" -domains \"${DOMAINS}\" -ip-addresses \"${IPADDRESSES}\""
-echo $cmd
+
 # generate the image
-docker build -t crs4/minica .
+docker build -f ${current_path}/Dockerfile -t ${IMAGE_NAME} ${current_path}
 
 # generate certs
 mkdir -p data
 docker run -it --rm \
     --user $(id -u):$(id -g) \
-    -v "${current_path}/data:/certs" crs4/minica \
+    -v "${current_path}/data:/certs" ${IMAGE_NAME} \
     /bin/bash -c "${cmd}"
