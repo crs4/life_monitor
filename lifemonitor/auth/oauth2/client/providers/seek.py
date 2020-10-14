4d2dbe794c71728d3c324ecd8b449d3a9a775068
Marco Enrico Piras <kikkomep@crs4.it>
2020-10-14 19:31:01 +0000

    Update seek normalized data

--- lifemonitor/auth/oauth2/client/providers/seek.py
+++ lifemonitor/auth/oauth2/client/providers/seek.py
@@ -41,8 +41,8 @@ class Seek(OAuth2IdentityProvider):
         params = {
             'sub': str(data['id']),
             'name': data['attributes']['title'],
-            'email': data['attributes']['mbox_sha1sum'],  # TODO: check if it is possible to decode the email
-            'preferred_username': data['id'],  # TODO: check if the username can be retrived from API
+            'mbox_sha1sum': data['attributes']['mbox_sha1sum'],
+            # 'preferred_username': data['id'],
             'profile': data['links']["self"],
             'picture': data['attributes']['avatar'],
             'website': '',
