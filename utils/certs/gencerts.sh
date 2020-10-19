9b197d6f2502c45c4d2e55776a30f015167f20c0
Marco Enrico Piras <kikkomep@crs4.it>
2020-10-19 20:26:57 +0000

    Cleanup output dir before regenerating certs

--- utils/certs/gencerts.sh
+++ utils/certs/gencerts.sh
@@ -16,6 +16,7 @@ cmd="minica -ca-cert \"${CA_NAME}.pem\" -ca-key \"${CA_NAME}.key\" -domains \"${
 docker build -f ${current_path}/Dockerfile -t ${IMAGE_NAME} ${current_path}
 
 # generate certs
+rm -rf "${current_path}/data"
 mkdir -p "${current_path}/data"
 docker run --rm \
     --user $(id -u):$(id -g) \
