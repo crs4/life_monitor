cde4e766e64272029723509c97d86450b4e34783
Marco Enrico Piras <kikkomep@crs4.it>
2021-03-02 18:02:53 +0000

    Fix missing import

--- lifemonitor/api/services.py
+++ lifemonitor/api/services.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from typing import Union
+from typing import Union, List
 
 import lifemonitor.exceptions as lm_exceptions
 from lifemonitor.api import models
