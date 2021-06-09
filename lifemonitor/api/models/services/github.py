98bda79a557c67491d198f99ce2b4d121a968b10
Luca Pireddu <pireddu@crs4.it>
2021-06-09 08:25:56 +0200

    Working GitHub test service implementation

--- lifemonitor/api/models/services/github.py
+++ lifemonitor/api/models/services/github.py
@@ -35,11 +35,11 @@ import lifemonitor.exceptions as lm_exceptions
 from .service import TestingService
 
 # set module level logger
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 
 class GithubTestingService(TestingService):
-    _server = None
+    _gh_obj = None
     __mapper_args__ = {
         'polymorphic_identity': 'github_testing_service'
     }
@@ -67,13 +67,27 @@ class GithubTestingService(TestingService):
         TIMED_OUT = 'timed_out'
 
     def __init__(self, url: str = None, token: models.TestingServiceToken = None) -> None:
+        logger.debug("GithubTestingService constructor instantiating client")
         if not url:
             url = github.MainClass.DEFAULT_BASE_URL
         super().__init__(url, token)
+        logger.debug("url: %s; token: %s\nClient configuration: %s",
+                     url, token, self._configuration_)
         try:
-            self._server = Github(base_url=url, **self._configuration_)
+            self._gh_obj = Github(base_url=url, **self._configuration_)
+            logger.debug("Github client created.")
         except Exception as e:
             raise lm_exceptions.TestingServiceException(e)
+
+    @property
+    def _gh_service(self) -> Github:
+        logger.debug("Github client requested.")
+        if not self._gh_obj:
+            logger.debug("Instantiating with: url %s; token: %s\nClient configuration: %s",
+                         self.url, None, self._configuration_)
+            self._gh_obj = Github(base_url=self.url, **self._configuration_)
+            logger.debug("Github client created.")
+        return self._gh_obj
 
     @staticmethod
     def _convert_github_exception_to_lm(github_exc: GithubException) -> lm_exceptions.LifeMonitorException:
@@ -88,7 +102,9 @@ class GithubTestingService(TestingService):
         try:
             # Call the GET /rate_limit API to test the connection. Seems to be the
             # simplest call with a small, constant-size result
-            self._server.get_rate_limit()
+            self._gh_service.get_rate_limit()
+            logger.debug("GithubTestingService:  check_connection() -> seems ok")
+            self._gh_service.get_rate_limit()
             return True
         except GithubException as e:
             logger.info("Caught exception from Github GET /rate_limit: %s.  Connection not working?", e)
@@ -96,9 +112,11 @@ class GithubTestingService(TestingService):
 
     def _iter_runs(self, test_instance: models.TestInstance, status: str = None) -> Generator[github.WorkflowRun.WorkflowRun]:
         _, repository, workflow_id = self._parse_workflow_url(test_instance.resource)
+        logger.debug("iterating over runs --  wf id: %s; repository: %s; status: %s", workflow_id, repository, status)
 
         status_arg = status if status else github.GithubObject.NotSet
-        workflow = self._server.get_repo(repository).get_workflow(workflow_id)
+        workflow = self._gh_service.get_repo(repository).get_workflow(workflow_id)
+        logger.debug("Retrieved workflow %s from github", workflow_id)
         for run in workflow.get_runs(status=status_arg):
             yield run
 
@@ -137,28 +155,29 @@ class GithubTestingService(TestingService):
 
     def _parse_workflow_url(self, resource: str) -> Tuple[str, str, str]:
         """
-        Given a URL to the testing GitHub Worklflow, returns a tuple
+        Given a URL to the testing Github Worklflow, returns a tuple
         (server, repository, workflow_id)
         """
-        # URL identifies a GitHub Workflow and should have the form:
+        # URL identifies a Github Workflow and should have the form:
         #   https://api.github.com/repos/crs4/life_monitor/actions/workflows/4094661
         expected_url_msg = " Expected '/repos/{org}/{reponame}/actions/workflows/{workflow_id}'"
         try:
-            logging.debug("Parsing testing instance URL %s", resource)
+            logger.debug("Parsing testing instance resource URL '%s'", resource)
             result = urlparse(resource)
             server = f"{result.scheme}://{result.netloc}"
             # simply split the path using the / delimiter.  We'll later access the parts by index
             parts = result.path.split('/')
             # Validate our assumptions
             if len(parts) != 7:
-                raise RuntimeError("Unexpected number of parts in URL to GitHub Workflow." + expected_url_msg)
+                raise RuntimeError("Unexpected number of parts in URL to Github Workflow." + expected_url_msg)
             if parts[1] != 'repos':
-                raise RuntimeError("Missing or misplaced /repos in URL to GitHub Workflow." + expected_url_msg)
+                raise RuntimeError("Missing or misplaced /repos in URL to Github Workflow." + expected_url_msg)
             if parts[4] != 'actions' or parts[5] != 'workflows':
-                raise RuntimeError("Missing or misplaced /actions/workflows in URL to GitHub Workflow." + expected_url_msg)
+                raise RuntimeError("Missing or misplaced /actions/workflows in URL to Github Workflow." + expected_url_msg)
             # Extract info
             repository = '/'.join(parts[2:4])
             workflow_id = parts[6]
+            logger.debug("parse result -- server: '%s'; repository: '%s'; workflow_id: '%s'", server, repository, workflow_id)
             return server, repository, workflow_id
         except URLError as e:
             raise lm_exceptions.SpecificationNotValidException(
