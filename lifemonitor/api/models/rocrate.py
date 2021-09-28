e4219b70d0d1c181dbea26fb750fc5219a7c7207
Marco Enrico Piras <kikkomep@crs4.it>
2021-09-28 07:52:22 +0000

    Restore function call

--- lifemonitor/api/models/rocrate.py
+++ lifemonitor/api/models/rocrate.py
@@ -115,7 +115,7 @@ class ROCrate(Resource):
 
     def download(self, target_path: str) -> str:
         # report if the workflow is not longer available on the origin server
-        if self._metadata and not self.available:
+        if self._metadata and not check_resource_exists(self.uri):
             raise lm_exceptions.DownloadException(detail=f"Workflow not found on the origin: {self.uri}", status=410)
 
         errors = []
