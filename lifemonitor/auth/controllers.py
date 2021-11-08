77ca6a05cb404c3ad5d050a1e325d8ac65f294da
Marco Enrico Piras <kikkomep@crs4.it>
2021-11-08 17:06:17 +0000

    Set cache timeout of user profile controller to session

--- lifemonitor/auth/controllers.py
+++ lifemonitor/auth/controllers.py
@@ -23,7 +23,7 @@ import logging
 import flask
 from flask import flash, redirect, render_template, request, session, url_for
 from flask_login import login_required, login_user, logout_user
-from lifemonitor.cache import cached
+from lifemonitor.cache import cached, Timeout
 from lifemonitor.utils import (NextRouteRegistry, next_route_aware,
                                split_by_crlf)
 
@@ -50,7 +50,7 @@ login_manager.login_view = "auth.login"
 
 
 @authorized
-@cached(timeout=3600)
+@cached(timeout=Timeout.SESSION)
 def show_current_user_profile():
     try:
         if current_user and not current_user.is_anonymous:
@@ -66,7 +66,7 @@ def user_subscriptions_get():
 
 
 @authorized
-@cached()
+@cached(timeout=Timeout.REQUEST)
 def get_registry_users():
     try:
         if current_registry and current_user.is_anonymous:
@@ -81,7 +81,7 @@ def get_registry_users():
 
 
 @authorized
-@cached()
+@cached(timeout=Timeout.REQUEST)
 def get_registry_user(user_id):
     try:
         if current_registry:
@@ -97,6 +97,7 @@ def index():
 
 
 @blueprint.route("/profile", methods=("GET",))
+@cached(timeout=Timeout.SESSION)
 def profile(form=None, passwordForm=None, currentView=None):
     currentView = currentView or request.args.get("currentView", 'accountsTab')
     logger.debug(OpenApiSpecs.get_instance().authorization_code_scopes)
