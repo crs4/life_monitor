e45154929feedd138b9af5892ab64404ed06b77f
Marco Enrico Piras <kikkomep@crs4.it>
2021-11-08 17:07:07 +0000

    Fix and restore task to update cache

--- lifemonitor/tasks/tasks.py
+++ lifemonitor/tasks/tasks.py
@@ -41,16 +41,21 @@ def heartbeat():
     logger.info("Heartbeat!")
 
 
-# @schedule(CronTrigger(minute="*/5"))
-# @dramatiq.actor
-# def check_last_build():
-#     logger.info("Checking last build....")
-#     from lifemonitor.api.models import Workflow
+@schedule(CronTrigger(minute="*/5"))
+@dramatiq.actor
+def check_last_build():
+    logger.info("Checking last build....")
+    from lifemonitor.api.models import Workflow
 
-#     for w in Workflow.all():
-#         for s in w.latest_version.test_suites:
-#             #logger.info("Updating workflow: %r", w)
-#             for i in s.test_instances:
-#                 i.refresh()
+    for w in Workflow.all():
+        for s in w.latest_version.test_suites:
+            logger.info("Updating workflow: %r", w)
+            for i in s.test_instances:
+                try:
+                    i.ignore_cache_values = True
+                    logger.debug("Updating latest builds: %r", i.get_test_builds())
+                finally:
+                    i.ignore_cache_values = False
+                logger.debug("Updating latest build: %r", i.last_test_build)
 
-#     logger.info("Checking last build: DONE!")
+    logger.info("Checking last build: DONE!")
