cdf07b7c652e7d3eb9d6fa8db781399d3887b2c1
Marco Enrico Piras <kikkomep@crs4.it>
2021-12-02 22:11:20 +0100

    Fix workflow filter

--- lifemonitor/api/models/workflows.py
+++ lifemonitor/api/models/workflows.py
@@ -148,11 +148,12 @@ class Workflow(Resource):
         result: List[Workflow] = cls.query.join(Permission)\
             .filter(Permission.user_id == owner.id).all()
         if include_subscriptions:
-            result.extend(cls.query
-                          .join(Subscription).filter(Subscription.user_id == owner.id and Subscription.resource_id == cls.id)
-                          .filter(cls.public == true())
-                          .all())
-        return list({x.name: x for x in result}.values())
+            subscribed_workflows = cls.query\
+                .join(Subscription).filter(Subscription.user_id == owner.id and Subscription.resource_id == cls.id) \
+                .filter(cls.public == true()).all()
+            user_wf_ids = [w.uuid for w in result]
+            result.extend([w for w in subscribed_workflows if w.uuid not in user_wf_ids])
+        return result
 
     @classmethod
     def get_public_workflows(cls) -> List[Workflow]:
