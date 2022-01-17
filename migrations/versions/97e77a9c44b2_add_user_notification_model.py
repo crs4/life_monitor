cd0e06a366f3823ec202ccb58b8638115c4e6bc6
Marco Enrico Piras <kikkomep@crs4.it>
2022-01-17 16:27:25 +0100

    Update migration to support the notification model

--- migrations/versions/97e77a9c44b2_add_user_notification_model.py
+++ migrations/versions/97e77a9c44b2_add_user_notification_model.py
@@ -1,8 +1,8 @@
 """Add user notification model
 
-Revision ID: 3fcb14537658
+Revision ID: 97e77a9c44b2
 Revises: f4cbfe20075f
-Create Date: 2022-01-14 18:26:49.429440
+Create Date: 2022-01-17 13:21:37.565495
 
 """
 from alembic import op
@@ -11,7 +11,7 @@ from lifemonitor.models import JSON
 
 
 # revision identifiers, used by Alembic.
-revision = '3fcb14537658'
+revision = '97e77a9c44b2'
 down_revision = 'f4cbfe20075f'
 branch_labels = None
 depends_on = None
@@ -21,14 +21,19 @@ def upgrade():
     op.create_table('notification',
                     sa.Column('id', sa.Integer(), nullable=False),
                     sa.Column('created', sa.DateTime(), nullable=True),
-                    sa.Column('emailed', sa.DateTime(), nullable=True),
-                    sa.Column('read', sa.DateTime(), nullable=True),
                     sa.Column('name', sa.String(), nullable=True),
                     sa.Column('type', sa.String(), nullable=False),
                     sa.Column('data', JSON(), nullable=True),
-                    sa.Column('user_id', sa.Integer(), nullable=False),
-                    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
                     sa.PrimaryKeyConstraint('id')
+                    )
+    op.create_table('user_notification',
+                    sa.Column('emailed', sa.DateTime(), nullable=True),
+                    sa.Column('read', sa.DateTime(), nullable=True),
+                    sa.Column('user_id', sa.Integer(), nullable=False),
+                    sa.Column('notification_id', sa.Integer(), nullable=False),
+                    sa.ForeignKeyConstraint(['notification_id'], ['notification.id'], ),
+                    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
+                    sa.PrimaryKeyConstraint('user_id', 'notification_id')
                     )
     op.add_column('user', sa.Column('email', sa.String(), nullable=True))
     op.add_column('user', sa.Column('email_verification_hash', sa.String(length=256), nullable=True))
@@ -39,4 +44,5 @@ def downgrade():
     op.drop_column('user', 'email_verified')
     op.drop_column('user', 'email_verification_hash')
     op.drop_column('user', 'email')
+    op.drop_table('user_notification')
     op.drop_table('notification')
