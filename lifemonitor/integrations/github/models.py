22c20b6338edf680450ef2ccbbce4c994181f8fa
Marco Enrico Piras <kikkomep@crs4.it>
2022-03-01 16:50:56 +0100

    Refactor base Github App using models from PyGIthub

--- lifemonitor/integrations/github/models.py
+++ lifemonitor/integrations/github/models.py
@@ -28,32 +28,52 @@ from typing import List
 
 import jwt
 import requests
-from lifemonitor.cache import IllegalStateException
+from lifemonitor.exceptions import IllegalStateException, LifeMonitorException
 
-import github
+from github import Github
+from github import GithubIntegration as GithubIntegrationBase
+from github.GithubApp import GithubApp
+from github.Installation import Installation
+from github.InstallationAuthorization import InstallationAuthorization
+from github.Requester import Requester
+
+DEFAULT_BASE_URL = "https://api.github.com"
+DEFAULT_TIMEOUT = 15
+DEFAULT_PER_PAGE = 30
+DEFAULT_TOKEN_EXPIRATION = timedelta(seconds=60)
 
 # Config a module level logger
 logger = logging.getLogger(__name__)
 
 
-class GithubApp():
+class LifeMonitorGithubApp(GithubApp):
 
-    app_identifier = None
-    app_slug = 'lifemonitorlocal'
+    __instance__: LifeMonitorGithubApp = None
     _signing_key_path = None
     _signing_secret = None
-    token_expiration = timedelta(minutes=10)
-    github_api_url = 'https://api.github.com'
+    _app_identifier = None
 
     @classmethod
-    def init(cls, app_identifier: str, signing_key_path: str, signing_secret: str):
-        cls.app_identifier = app_identifier
+    def init(cls, app_identifier: str,
+             signing_key_path: str, signing_secret: str,
+             base_url: str = DEFAULT_BASE_URL,
+             token_expiration: timedelta = DEFAULT_TOKEN_EXPIRATION):
+        cls._app_identifier = app_identifier
         cls._signing_key_path = signing_key_path
         cls._signing_secret = signing_secret
+        if not cls.__instance__:
+            integration = GithubIntegration(cls._app_identifier, cls._get_signing_key(), base_url=base_url)
+            cls.__instance__ = cls(integration, base_url=base_url, token_expiration=token_expiration)
+
+    @classmethod
+    def get_instance(cls) -> LifeMonitorGithubApp:
+        if not cls.check_initialization():
+            raise IllegalStateException(detail="Github App not initializaed")
+        return cls.__instance__
 
     @classmethod
     def check_initialization(cls) -> bool:
-        if not cls.app_identifier or not cls._signing_key_path or not cls._signing_secret:
+        if not cls._app_identifier or not cls._signing_key_path or not cls._signing_secret:
             logger.warning("Github App integration not properly configured: "
                            "check GITHUB_INTEGRATION_* properties on your settings.conf")
             return False
@@ -81,125 +101,107 @@ class GithubApp():
         # See if they match
         return hmac.compare_digest(local_signature.hexdigest(), github_signature)
 
-    @classmethod
-    def get_app_client(cls) -> requests.Session:
-        message = {
-            # The time that this JWT was issued, _i.e._ now.
-            "iat": jwt.utils.get_int_from_datetime(datetime.now(timezone.utc)),
-            # JWT expiration time (10 minute maximum)
-            "exp": jwt.utils.get_int_from_datetime(datetime.now(timezone.utc) + cls.token_expiration),
-            # Your GitHub App's identifier number
-            "iss": cls.app_identifier
-        }
-        # Cryptographically sign the JWT
-        token = jwt.JWT().encode(message, cls._get_signing_key(), alg='RS256')
+    def __init__(self, integration: GithubIntegration,
+                 base_url=DEFAULT_BASE_URL, token_expiration=DEFAULT_TOKEN_EXPIRATION) -> None:
+        self._integration = integration
+        self.token_expiration = token_expiration
+        self.base_url = base_url
+        session = self.app_client_session()
+        app = self._get_app_info(session=session)
+        self._requester = __make_requester__(
+            jwt=self.integration.create_jwt(expiration=self.token_expiration),
+            base_url=self.base_url)
+        super().__init__(self._requester, session.headers, app, True)
+
+    def _get_app_info(self, session: requests.Session = None):
+        session = session or self.app_client_session()
+        response = session.get(f"{self.base_url}/app")
+        if response.status_code == 200:
+            return response.json()
+        raise LifeMonitorException(detail=response.content, status=response.status_code)
+
+    def app_client(self, expiration=None) -> Github:
+        return Github(jwt=self.integration.create_jwt(expiration=expiration or self.token_expiration))
+
+    def app_client_session(self, expiration=None) -> requests.Session:
         s = requests.Session()
         s.headers.update({
-            'Authorization': f"Bearer {token}",
+            'Authorization': f"Bearer {self.integration.create_jwt(expiration=expiration or self.token_expiration)}",
             'Accept': 'application/vnd.github.v3+json'
         })
         return s
 
-    @classmethod
-    def get_installation_token(cls, installation_id: str) -> str:
-        # Authenticate as installed app
-        app_client = cls.get_app_client()
-        response = app_client.post(f"{cls.github_api_url}/app/installations/{installation_id}/access_tokens")
-        logger.debug("Status code: %r" % response.status_code)
-        assert response.status_code == 201, "Unable to create access token"
-        return response.json()
-
-    @classmethod
-    def get_installation_client(cls, installation_id: str) -> GithubInstallationClient:
-        token = cls.get_installation_token(installation_id)
-        logger.debug(token)
-        return GithubInstallationClient(installation_id, token)
-
-    @classmethod
-    def get_github_client(cls, installation_id: str) -> github.Github:
-        token = cls.get_installation_token(installation_id)
-        return github.Github(token['token'])
-
-    @classmethod
-    def get_app(cls) -> object:
-        response = requests.get(f"{cls.github_api_url}/apps/{cls.app_slug}")
-        if response.status_code == 200:
-            return response.json()
-        logger.error(response.content)
-        return "Internal Error", 500
-
-    @classmethod
-    def get_installations(cls) -> List[object]:
-        app_client = cls.get_app_client()
-        response = app_client.get(f"{cls.github_api_url}/app/installations")
-        if response.status_code == 200:
-            return response.json()
-        logger.error(response.content)
-        return "Internal Error", 500
-
-    @classmethod
-    def get_installation(cls, installation_id: str) -> object:
-        app_client = cls.get_app_client()
-        response = app_client.get(f"{cls.github_api_url}/app/installations/{installation_id}")
-        if response.status_code == 200:
-            return response.json()
-        logger.error(response.content)
-        if response.status_code == 404:
-            return None
-        return "Internal Error", 500
-
-    @classmethod
-    def get_installation_by_org(cls, org: str) -> object:
-        app_client = cls.get_app_client()
-        response = app_client.get(f"{cls.github_api_url}/orgs/{org}/installation")
-        if response.status_code == 200:
-            return response.json()
-        logger.error(response.content)
-        if response.status_code == 404:
-            return None
-        return "Internal Error", 500
-
-    @classmethod
-    def get_installation_by_owner_repo(cls, owner: str, repo: str) -> object:
-        app_client = cls.get_app_client()
-        response = app_client.get(f"{cls.github_api_url}/repos/{owner}/{repo}/installation")
-        if response.status_code == 200:
-            return response.json()
-        logger.error(response.content)
-        if response.status_code == 404:
-            return None
-        return "Internal Error", 500
-
-
-class GithubInstallationClient():
-
-    _api_url = GithubApp.github_api_url
-
-    def __init__(self, installation_id: str, token: object) -> None:
-        self._installation_id = installation_id
-        self._token = token
-        self._session = requests.Session()
-        self._session.headers.update({
-            'Authorization': f"Bearer {self._token['token']}",
-            'Accept': 'application/vnd.github.v3+json'
-        })
+    @property
+    def integration(self) -> GithubIntegration:
+        return self._integration
 
     @property
-    def session(self) -> requests.Session:
-        return self._session
-
-    @property
-    def installation_id(self) -> str:
-        return self._installation_id
-
-    @property
-    def installation(self) -> object:
-        return GithubApp.get_installation(self._installation_id)
-
-    @property
-    def installation_repositories(self) -> List[object]:
-        response = self._session.get(f"{self._api_url}/installation/repositories")
+    def installations(self) -> List[LifeMonitorInstallation]:
+        app_client = self.app_client_session()
+        response = app_client.get(f"{self.base_url}/app/installations")
         if response.status_code == 200:
-            return response.json()['repositories']
+            return [LifeMonitorInstallation(self, i, requester=self._requester) for i in response.json()]
         logger.error(response.content)
         return "Internal Error", 500
+
+    def get_installation(self, installation_id: str) -> LifeMonitorInstallation:
+        logger.debug("Searching installation_id %r", installation_id)
+        for i in self.installations:
+            logger.debug("Installation: %r %r %r", i, i.id, i.app)
+            if str(i.id) == str(installation_id):
+                return i
+        return None
+
+
+class GithubIntegration(GithubIntegrationBase):
+
+    def create_jwt(self, expiration=timedelta(seconds=20)) -> str:
+        now = datetime.now(timezone.utc)
+        message = {
+            # The time that this JWT was issued, _i.e._ now.
+            "iat": jwt.utils.get_int_from_datetime(now),
+            # JWT expiration time (10 minute maximum)
+            "exp": jwt.utils.get_int_from_datetime(now + expiration),
+            # Your GitHub App's identifier number
+            "iss": self.integration_id
+        }
+        logger.debug("Token message: %r", message)
+        # Cryptographically sign the JWT
+        return jwt.JWT().encode(message, self.private_key, alg='RS256')
+
+
+class LifeMonitorInstallation(Installation):
+
+    def __init__(self, app: LifeMonitorGithubApp, data: object, requester=None, headers={}, completed=True):
+        super().__init__(requester, headers, data, completed)
+        self.app = app
+        self._auth: InstallationAuthorization = None
+
+    @property
+    def auth(self) -> InstallationAuthorization:
+        generate_token = False
+        if not self._auth:
+            generate_token = True
+        elif jwt.utils.get_int_from_datetime(self._auth.expires_at) < jwt.utils.get_int_from_datetime(datetime.now()):
+            generate_token = True
+        if generate_token:
+            self._auth = self.app.integration.get_access_token(self.id)
+            assert isinstance(self._auth, InstallationAuthorization), "Invalid authorization"
+        return self._auth
+
+    @property
+    def _requester(self):
+        if self.__requester:
+            self.__requester = __make_requester__(token=self.auth.token, base_url=self.app.base_url)
+        return self.__requester
+
+    @_requester.setter
+    def _requester(self, value: Requester):
+        self.__requester = value
+
+
+def __make_requester__(jwt: str = None, token: str = None, base_url: str = DEFAULT_BASE_URL) -> Requester:
+    assert jwt or token, "Auth JWT or TOKEN should be set"
+    return Requester(token, None, jwt, base_url,
+                     DEFAULT_TIMEOUT, "PyGithub/Python", DEFAULT_PER_PAGE,
+                     True, None, None)
