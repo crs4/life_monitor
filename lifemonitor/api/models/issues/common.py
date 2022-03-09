dce15b3816fcdb4a345827ed849c30ed5fbaf1d9
Marco Enrico Piras <kikkomep@crs4.it>
2022-03-09 18:41:57 +0100

    Add initial implementation of some common issues

---
+++ lifemonitor/api/models/issues/common.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2020-2021 CRS4
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import annotations
+import json
+
+import logging
+
+from lifemonitor.api.models.repositories import (RepositoryFile,
+                                                 WorkflowRepository)
+
+from . import WorkflowRepositoryIssue
+
+# set module level logger
+logger = logging.getLogger(__name__)
+
+
+class MissingWorkflowFile(WorkflowRepositoryIssue):
+    name = "Missing workflow file"
+    description = "No workflow found on this repository.<br>"\
+        "You should place the workflow file (e.g., <code>.ga</code> file) on the root of this repository."
+    labels = ['invalid', 'enhancement']
+
+    def check(self, repo: WorkflowRepository) -> bool:
+        return repo.find_workflow() is None
+
+
+class MissingMetadataFile(WorkflowRepositoryIssue):
+    name = "Missing RO-Crate metadata"
+    description = "No <code>ro-crate-metadata.json</code> found on this repository.<br>"\
+        "The <code>ro-crate-metadata.json</code> should be placed on the root of this repository."
+    labels = ['invalid', 'enhancement']
+    depends_on = [MissingWorkflowFile]
+
+    def check(self, repo: WorkflowRepository) -> bool:
+        if repo.metadata is None:
+            repo.make_crate()
+            self.add_change(RepositoryFile(repo.metadata.metadata.id,
+                                           content=json.dumps(repo.metadata.to_json(), indent=4) + '\n'))
+            return True
+        return False
+
+
+class OutdatedMetadataFile(WorkflowRepositoryIssue):
+    name = "RO-Crate metadata outdated"
+    description = "The <code>ro-crate-metadata.json</code> needs to be updated."
+    labels = ['invalid', 'bug']
+    depends_on = [MissingMetadataFile]
+
+    def check(self, repo: WorkflowRepository) -> bool:
+        return False
