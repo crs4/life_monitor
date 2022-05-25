1c76a36291592d3d79e8ef963e8573653ba8f77b
Marco Enrico Piras <kikkomep@crs4.it>
2022-05-25 12:53:16 +0200

    Fix extraction of repo ref from event

--- lifemonitor/integrations/github/events.py
+++ lifemonitor/integrations/github/events.py
@@ -285,6 +285,10 @@ class GithubRepositoryReference(object):
         if not self._repo:
             repo = self.event.installation.get_repo(self.full_name)
             repo.ref = self.ref
+            if self.branch:
+                repo.ref = f"refs/heads/{self.branch}"
+            elif self.tag:
+                repo.ref = f"refs/tags/{self.tag}"
             repo.rev = self.rev
             self._repo = repo
         return self._repo
