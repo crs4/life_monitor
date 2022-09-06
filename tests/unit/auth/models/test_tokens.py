08112698b5e53332c881e329665811513b41cb76
Marco Enrico Piras <kikkomep@crs4.it>
2022-09-06 13:55:44 +0200

    Test token refresh

--- tests/unit/auth/models/test_tokens.py
+++ tests/unit/auth/models/test_tokens.py
@@ -19,11 +19,14 @@
 # SOFTWARE.
 
 import logging
+import threading
 import time
+from typing import List
 from unittest.mock import PropertyMock, patch
 
 import pytest
-from lifemonitor.auth.oauth2.client.models import OAuth2Token
+from lifemonitor.auth.oauth2.client.models import OAuth2Token, OAuthIdentity
+from lifemonitor.db import db
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
@@ -105,3 +108,58 @@ def test_fetch_token_on_not_refreshable_token_expired(token_property, user_ident
     fetched_token = user_identity.fetch_token()
     logger.debug("Fetched token: %r", fetched_token)
     assert fetched_token == current_token, "DB and fetched tokens should be equal"
+
+
+def update_user_profile_token(app, user_identity: OAuthIdentity, results: List, index: int):
+    with app.app_context():
+        # reload user identity
+        user_identity = OAuthIdentity.find_by_provider_user_id(user_identity.provider_user_id, user_identity.provider.name)
+        # get token
+        token = user_identity._token
+        # Force token expiration
+        app.config['OAUTH2_REFRESH_TOKEN_BEFORE_EXPIRATION'] = 0
+        token['expires_at'] = time.time()
+        # try to refresh the token
+        user_identity.refresh_token()
+        logger.info("Thread data before: %r", results)
+        results[index]['result'].append(user_identity._token)
+        logger.info("Thread data after: %r", results)
+        logger.info("Thread %r finished", index, )
+
+
+def test_fetch_token_multi_threaded(app_context, redis_cache, user_identity: OAuthIdentity):
+    # set the
+    logger.debug(user_identity)
+    token = user_identity._token
+
+    # set up threads
+    results = []
+    number_of_threads = 3
+    for index in range(number_of_threads):
+        t = threading.Thread(
+            target=update_user_profile_token, name=f"T{index}", args=(app_context.app, user_identity, results, index),
+            kwargs={})
+        results.append({
+            't': t,
+            'index': index,
+            "result": []
+        })
+        t.start()
+        time.sleep(2)
+
+    # wait for results
+    for tdata in results:
+        t = tdata['t']
+        t.join()
+    # check results
+    time.sleep(2)
+    # reload user identity
+    db.session.refresh(user_identity)
+    updated_token = user_identity._token
+    assert updated_token != token, "The token should be refreshed"
+    logger.debug("Intial token: %r", token)
+    logger.debug("Updated token: %r", updated_token)
+    # check that every thread updates the token
+    for t in range(number_of_threads - 1):
+        logger.debug(f"Tokens updated by thread '{t}' and '{t+1}' should be different", results[t]['result'][0], results[t + 1]['result'][0])
+        assert results[t]['result'][0] != results[t + 1]['result'][0], f"Tokens updated by thread '{t}' and '{t+1}' should be different"
