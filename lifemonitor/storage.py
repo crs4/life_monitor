0a1e59bda844a914555add760f332947ceaae418
Marco Enrico Piras <kikkomep@crs4.it>
2022-09-24 17:14:54 +0200

    Refactor storage class

--- lifemonitor/storage.py
+++ lifemonitor/storage.py
@@ -20,6 +20,8 @@
 
 from __future__ import annotations
 
+import functools
+
 import logging
 import os
 from typing import Dict, Optional
@@ -33,6 +35,18 @@ from lifemonitor.tasks.scheduler import Scheduler
 
 # set module level logger
 logger = logging.getLogger(__name__)
+
+
+def check_config(func):
+    @functools.wraps(func)
+    def check_config_impl(*args, **kwargs):
+        storage: RemoteStorage = args[0]
+        assert isinstance(storage, RemoteStorage), "Invali argument"
+        if not storage._enabled:
+            logger.warning("S3 Storage not properly configured")
+            return False
+        return func(*args, **kwargs)
+    return check_config_impl
 
 
 class RemoteStorage():
@@ -91,76 +105,66 @@ class RemoteStorage():
             self.__bucket = self._s3.Bucket(self.bucket_name)
         return self.__bucket
 
+    @check_config
     def exists(self, path: str) -> bool:
-        if not self._enabled:
-            logger.warning("S3 Storage not properly configured")
-            return False
-        else:
-            try:
-                self._s3.Object(self.bucket_name, path).load()
-                return True
-            except botocore.exceptions.ClientError as e:
-                if logger.isEnabledFor(logging.DEBUG):
-                    logger.exception(e)
-                if e.response['Error']['Code'] == "404":
-                    return False
+        try:
+            self._s3.Object(self.bucket_name, path).load()
+            return True
+        except botocore.exceptions.ClientError as e:
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.exception(e)
+            if e.response['Error']['Code'] == "404":
+                return False
         return False
 
+    @check_config
     def get_file(self, remote_path: str, local_path: str) -> bool:
-        if not self._enabled:
-            logger.warning("S3 Storage not properly configured")
-        else:
-            os.makedirs(os.path.dirname(local_path), exist_ok=True)
-            try:
-                self._get_bucket().download_file(remote_path, local_path)
-                return True
-            except Exception as e:
-                logger.error("Local path: %r", local_path)
-                if logger.isEnabledFor(logging.DEBUG):
-                    logger.exception(e)
+        os.makedirs(os.path.dirname(local_path), exist_ok=True)
+        try:
+            self._get_bucket().download_file(remote_path, local_path)
+            return True
+        except Exception as e:
+            logger.error("Local path: %r", local_path)
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.exception(e)
         return False
 
+    @check_config
     def put_file_as_job(self, local_path: str, remote_path: str):
         scheduler: Scheduler = self.app.scheduler
         logger.debug("Current app scheduler: %r", scheduler)
         scheduler.run_job('put_file', self.bucket_name, local_path, remote_path)
 
+    @check_config
     def put_file(self, local_path: str, remote_path: str):
-        if not self._enabled:
-            logger.warning("S3 Storage not properly configured")
-        else:
-            if not self.exists(remote_path):
-                with cache.lock(remote_path, timeout=Timeout.NONE) as lock:
-                    if not self.exists(remote_path):
-                        try:
-                            self._get_bucket().upload_file(local_path, remote_path)
-                        except Exception as e:
-                            if logger.isEnabledFor(logging.DEBUG):
-                                logger.exception(e)
-                            raise RuntimeError(e)
+        if not self.exists(remote_path):
+            with cache.lock(remote_path, timeout=Timeout.NONE) as lock:
+                if not self.exists(remote_path):
+                    try:
+                        self._get_bucket().upload_file(local_path, remote_path)
+                    except Exception as e:
+                        if logger.isEnabledFor(logging.DEBUG):
+                            logger.exception(e)
+                        raise RuntimeError(e)
 
+    @check_config
     def delete_folder(self, remote_path: str) -> bool:
-        if not self._enabled:
-            logger.warning("S3 Storage not properly configured")
-        else:
-            try:
-                self._get_bucket().objects.filter(Prefix=remote_path).delete()
-                return True
-            except Exception as e:
-                logger.error("Error when deleting path: %r", remote_path)
-                if logger.isEnabledFor(logging.DEBUG):
-                    logger.exception(e)
+        try:
+            self._get_bucket().objects.filter(Prefix=remote_path).delete()
+            return True
+        except Exception as e:
+            logger.error("Error when deleting path: %r", remote_path)
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.exception(e)
         return False
 
+    @check_config
     def delete_file(self, remote_path: str) -> bool:
-        if not self._enabled:
-            logger.warning("S3 Storage not properly configured")
-        else:
-            try:
-                self._get_bucket().Object(remote_path).delete()
-                return True
-            except Exception as e:
-                logger.error("Error when deleting path: %r", remote_path)
-                if logger.isEnabledFor(logging.DEBUG):
-                    logger.exception(e)
+        try:
+            self._get_bucket().Object(remote_path).delete()
+            return True
+        except Exception as e:
+            logger.error("Error when deleting path: %r", remote_path)
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.exception(e)
         return False
