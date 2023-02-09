af75cb911a9ed98a64bec770059798663a6b6588
Marco Enrico Piras <kikkomep@crs4.it>
2023-02-09 17:19:51 +0100

    feat(controller): add support for yaml and JSON

--- lifemonitor/schemas/controller.py
+++ lifemonitor/schemas/controller.py
@@ -6,7 +6,8 @@ import yaml
 from flask import jsonify, request
 
 from lifemonitor.cache import Timeout, cached
-from lifemonitor.schemas.validators import ConfigFileValidator, Validator
+from lifemonitor.exceptions import BadRequestException
+from lifemonitor.schemas.validators import ConfigFileValidator
 
 # Config a module level logger
 logger = logging.getLogger(__name__)
@@ -19,6 +20,15 @@ def lifemonitor_json():
 
 
 def validate():
-    data = yaml.unsafe_load(request.data)
+    data = None
+    logger.debug("Request: data", request.data)
+    try:
+        data = yaml.unsafe_load(request.data)
+    except yaml.parser.ParserError:
+        data = json.loads(request.data.decode())
+        logger.debug("JSON data: %r", data)
+    finally:
+        if not data:
+            raise BadRequestException(title="Invalid file format", detail="It should be a JSON or YAML file")
     logger.debug("Data: %r", data)
     return ConfigFileValidator.validate(data).to_dict()
