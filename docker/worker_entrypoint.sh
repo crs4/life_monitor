290964d128062d20f86bf9da75ec42aec89573e9
Marco Enrico Piras <kikkomep@crs4.it>
2023-02-22 12:24:44 +0100

    feat(work): allow to set debug mode through env

--- docker/worker_entrypoint.sh
+++ docker/worker_entrypoint.sh
@@ -13,7 +13,7 @@ function log() {
   printf "%s [worker_entrypoint] %s\n" "$(date +"%F %T")" "${*}" >&2
 }
 
-# DEBUG="${DEBUG:-0}"
+DEBUG="${DEBUG:-0}"
 FLASK_ENV="${FLASK_ENV:-production}"
 if [[ "${FLASK_ENV}" == "development" ]]; then
   DEBUG="${DEBUG:-1}"
