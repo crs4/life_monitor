0c37f1aca9eb8aefff83c0c69cf434fb3ced5f69
Marco Enrico Piras <kikkomep@crs4.it>
2023-02-27 10:15:43 +0100

    feat(core): bootstrap SocketIO handler

--- app.py
+++ app.py
@@ -18,10 +18,17 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import functools
+import logging
 import os
 import ssl
+from typing import Dict
+
+from flask_socketio import SocketIO, disconnect
 
 from lifemonitor.app import create_app
+
+logger = logging.getLogger(__name__)
 
 # create an app instance
 application = create_app()
@@ -32,4 +39,45 @@ if __name__ == '__main__':
     context.load_cert_chain(
         os.environ.get("LIFEMONITOR_TLS_CERT", './certs/lm.crt'),
         os.environ.get("LIFEMONITOR_TLS_KEY", './certs/lm.key'))
-    application.run(host="0.0.0.0", port=8000, ssl_context=context)
+
+    # init SocketIO middleware
+    socketIO = SocketIO(application, cors_allowed_origins="*")
+
+    def authenticated_only(f):
+        @functools.wraps(f)
+        def wrapped(*args, **kwargs):
+            logger.debug("args: %r", args)
+            logger.debug("kwargs: %r", kwargs)
+            payload: Dict = args[0] if len(args) > 0 else None  # type: ignore
+            if 'token' not in payload:
+                logger.debug("Disconnecting...")
+                disconnect()
+            else:
+                logger.debug("Connection accepted....")
+                return f(payload)
+        return wrapped
+
+    @socketIO.on('connect')
+    def connect(auth):
+        logger.debug("Connected: %r", auth)
+        return True
+
+    @socketIO.on('message')
+    def handle_message(data):
+        logger.error('received message: %r' % data)
+        from flask_socketio import emit
+        emit("server message", ({"data": 12}), namespace="/", broadcast=True)
+
+    from eventlet.green.OpenSSL import SSL
+
+    # insecure context, only for example purposes
+    context = SSL.Context(SSL.SSLv23_METHOD)
+    # Pass server's private key created
+    context.use_privatekey_file(os.environ.get("LIFEMONITOR_TLS_KEY", './certs/lm.key'))
+    # Pass self-signed certificate created
+    context.use_certificate_file(os.environ.get("LIFEMONITOR_TLS_CERT", './certs/lm.crt'))
+
+    # Start Flask App + SocketIO
+    socketIO.run(application, host="0.0.0.0", port=8000, debug=True,
+                 keyfile=os.environ.get("LIFEMONITOR_TLS_KEY", './certs/lm.key'),
+                 certfile=os.environ.get("LIFEMONITOR_TLS_CERT", './certs/lm.crt'))
