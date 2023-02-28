ccccc082d51bd764cd777e7ca50d974a0cd8d9c2
Marco Enrico Piras <kikkomep@crs4.it>
2023-02-28 22:45:04 +0100

    fix resolve cyclic dependency

--- lifemonitor/tasks/jobs/__init__.py
+++ lifemonitor/tasks/jobs/__init__.py
@@ -2,13 +2,12 @@ import logging
 import os
 from typing import List
 
-from lifemonitor.utils import load_modules
-
 # set module level logger
 logger = logging.getLogger(__name__)
 
 
 def load_job_modules(include: List[str] = None, exclude: List[str] = None):
+    from lifemonitor.utils import load_modules
     __job_modules__ = load_modules(os.path.dirname(__file__), include=include, exclude=exclude)
     logger.debug("Loaded job modules: %r", __job_modules__)
     return __job_modules__
