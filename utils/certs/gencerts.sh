6c44285c9453105b2c2262ac3fc2e2def737e624
Marco Enrico Piras <kikkomep@crs4.it>
2023-06-12 08:42:18 +0200

    feat: use `gsed` (GNU sed) on macOS

--- utils/certs/gencerts.sh
+++ utils/certs/gencerts.sh
@@ -10,7 +10,18 @@ else
     NETWORK_DATA="$(ip -oneline addr)"
 fi
 
-IPADDRESSES=$(echo "${NETWORK_DATA}" | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p;' | sed -e ':a;N;$!ba;s/\n/,/g')
+# check if GNU sed is available
+gsed=sed
+if uname -a | grep -q Darwin; then
+    if ! type -P gsed > /dev/null 2>&1; then
+        echo "GNU sed is not available. Please install it with 'brew install gnu-sed'" >&2
+        exit 1
+    else
+        gsed=gsed
+    fi
+fi
+
+IPADDRESSES=$(echo "${NETWORK_DATA}" | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p;' | ${gsed} -e ':a;N;$!ba;s/\n/,/g')
 DOMAINS="lm,lm.local,lifemonitor,lifemonitor.local,lmtests,localhost,seek,nginx,wfhub"
 IMAGE_NAME="crs4/minica"
 
