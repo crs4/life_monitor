527ef692fbb81da625445e95dd421c63d43efef9
Marco Enrico Piras <kikkomep@crs4.it>
2023-11-16 18:03:28 +0100

    fix: update worker init in maintenance mode

--- lifemonitor/tasks/worker.py
+++ lifemonitor/tasks/worker.py
@@ -5,9 +5,13 @@ from ..app import create_app
 
 logger = logging.getLogger(__name__)
 
-
 app = create_app(worker=True, load_jobs=True)
 app.app_context().push()
 
-
-broker = app.broker
+# check if the app is in maintenance mode
+if app.config.get("MAINTENANCE_MODE", False):
+    logger.warning("Application is in maintenance mode")
+    app.run()
+else:
+    # initialise the message broker
+    broker = app.broker
