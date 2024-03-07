3d9b20aec72214b18ff3fca41d9bc4a0fbbc35bb
Marco Enrico Piras <kikkomep@crs4.it>
2024-03-07 17:55:00 +0100

    fix(gh-app): :fire: remove unused import

--- lifemonitor/integrations/github/controllers.py
+++ lifemonitor/integrations/github/controllers.py
@@ -41,8 +41,6 @@ from lifemonitor.api.models.repositories.github import GithubWorkflowRepository
 from lifemonitor.api.models.testsuites.testinstance import TestInstance
 from lifemonitor.api.models.wizards import QuestionStep, UpdateStep
 from lifemonitor.api.models.workflows import WorkflowVersion
-from lifemonitor.auth.models import User
-from lifemonitor.auth.oauth2.client.models import OAuthIdentity
 from lifemonitor.auth.services import authorized, current_user
 from lifemonitor.integrations.github import pull_requests
 from lifemonitor.integrations.github.app import LifeMonitorGithubApp
